a551ccd0df86468fad66ab10f53c239efea05a02
Manuel Humberto Velarde Flores <171918@unsaac.edu.pe>
2021-08-22 23:12:41 -0500

    Update clustering.r

--- Clustering/clustering.r
+++ Clustering/clustering.r
@@ -6,52 +6,51 @@ library(tm)
 library(qdap)
 require(openNLP)
 
-c <- Corpus(DirSource("D:/CLASES/2021-I/Procesamiento de Lenguaje/Corpus-LDA/Corpus"))
+# Lectura de Archivo
+c <- Corpus(DirSource("Aquí va la dirección de su archivo"))
 summary(c)
 
-#criar a matriz tf-idf
-#dtm <- DocumentTermMatrix(c, control=list(wordLengths=c(3,Inf),weighting = weightTfIdf))
+# Creación de matriz tf-idf
+# dtm <- DocumentTermMatrix(c, control=list(wordLengths=c(3,Inf),weighting = weightTfIdf))
 dtm <- DocumentTermMatrix(c)
 x<-as.matrix(dtm)
 rownames(x) <- paste(substring(rownames(x),1,5))
 
-#distancia
+# Distancia
 m <- as.matrix(dist(x))
 d <- as.dist(m)
 stopifnot(d == dist(x))
 
-#Clustering
-#saving trace
-write.csv(x,file="dtm-before-shorten-tf.csv")
 
-#dendogram
+# Archivo para guardar la matriz de términos de documento
+write.csv(x,file="dtm-Transparencia.csv")
+
+# Dendograma
 groups <- hclust(d,method="ward.D")
 plot(groups, hang=-1)
 rect.hclust(groups,2)
 
-#clustering
-#k means algorithm, 2 clusters, 100 starting configurations
-kfit <- kmeans(d, 4, nstart=100)
-#plot â€“ need library cluster
+# Clustering
+# Algoritmo kmeans, 2 clusters, configuración inicial = 100
+kfit <- kmeans(d, 2, nstart=100)
 library(cluster)
 clusplot(m, kfit$cluster, color=T, shade=T, labels=2, lines=0)
 
 
-#find number of clusters
+# Hallar el número óptimo de clusters
 m.scaled<-scale(x)
 k.max <- 15
 data <- m.scaled
 sil <- rep(0, k.max)
-
-# Compute the average silhouette width for 
-# k = 2 to k = 15
+# Calcular el ancho de silueta promedio para 
+# k = 2 a k = 5
 for(i in 2:k.max){
-  km.res <- kmeans(data, centers = i, nstart = 25, iter.max=30)#https://stat.ethz.ch/pipermail/r-help/2006-March/102752.html
+  km.res <- kmeans(data, centers = i, nstart = 25, iter.max=30)
   ss <- silhouette(km.res$cluster, dist(data))
   sil[i] <- mean(ss[, 3])
 }
 
-# Plot the  average silhouette width
+# Mostrar el ancho de silueta promedio
 plot(1:k.max, sil, type = "b", pch = 19, 
-     frame = FALSE, xlab = "Number of clusters k")
+     frame = FALSE, xlab = "Número de clusters k")
 abline(v = which.max(sil), lty = 2)
